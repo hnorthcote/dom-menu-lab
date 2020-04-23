425915c8bdc3c65506cdd2f8e83045c29096e1f4
Henry Northcote <hnorthcote2000@hotmail.com>
2020-04-22 21:32:33 -0600

    Starting 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-let mainEl =  document.getElementById('main');
-mainEl.style.backgroundColor= :root(--main-bg);
-mainEl.textContent="<h1>SEI Rocks!</h1>;
-mainEl.classList.add="flex-ctr"
+
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+let mainEl =  document.querySelector('main');
+let topMenuEl = document.getElementById('top-menu');
+
+mainEl.style.backgroundColor=("var(--main-bg)");
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>"
+mainEl.classList.add("flex-ctr");
+topMenuEl.style.height=('100%');
+topMenuEl.style.backgroundColor=("var(--top-menu-bg)");
+topMenuEl.classList.add("flex-around");
+
+menuLinks.forEach(function addAtag() {
+
+})
+

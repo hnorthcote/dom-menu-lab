4f54a8476f75dbd85288c0f5dd107ea65e03eafd
hnorthcote <hnorthcote2000@hotmail.com>
2020-04-27 10:16:23 -0600

    Troubleshooting topMenuLinks

--- js/script.js
+++ js/script.js
@@ -1,28 +1,78 @@
 
-
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
 let mainEl =  document.querySelector('main');
 let topMenuEl = document.getElementById('top-menu');
+let subMenuEl = document.getElementById('sub-menu');
+let topMenuLinks = document.querySelectorAll('a');
+
+
 //Task 1
-mainEl.style.backgroundColor=("var(--main-bg)");
-mainEl.innerHTML = "<h1>SEI Rocks!</h1>"
-mainEl.classList.add("flex-ctr");
+mainEl.style.backgroundColor=('var(--main-bg)');
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+mainEl.classList.add('flex-ctr');
 
 //Task2
 topMenuEl.style.height=('100%');
-topMenuEl.style.backgroundColor=("var(--top-menu-bg)");
-topMenuEl.classList.add("flex-around");
+topMenuEl.style.backgroundColor=('var(--top-menu-bg)');
+topMenuEl.classList.add('flex-around');
  //Task 3
 menuLinks.forEach( function (aTag) {
    let aEl = document.createElement('a');    
-  aEl.innerText = aTag.text
+  aEl.textContent = aTag.text;
   aEl.setAttribute('href', aTag.href);
   topMenuEl.appendChild(aEl);
+  return;
 });
+console.log(topMenuLinks);
+//Task 4
+subMenuEl.style.height=('100%');
+subMenuEl.style.backgroundColor=('var(--sub-menu-bg)');
+subMenuEl.classList.add('flex-around');
+subMenuEl.style.position=('absolute');
+subMenuEl.style.top=('0');
+//task 5
+
+var showingSubMenu = false;
+topMenuEl.addEventListener('click', topMenuClick);
+
+function topMenuClick(evt) {
+  evt.preventDefault();
+  
+  if (evt.target.tagName !== 'A') 
+     return;
+console.log(event.target.textContent);
+  if (evt.target.classList.contains("active")) {
+    evt.target.classList.remove('active');
+    showingSubMenu = false;
+    subMenuEl.style.top= 0;
+    return;
+  }
+
+
+    for(let i = 0; i < topMenuLinks.length; i++){
+        console.log('check');
+      topMenuLinks[i].classList.remove('active');
+    }
+
+  console.log(evt.target.classList.contains('active'));
+      evt.target.classList.add('active');
+}
+// 5.5
 
